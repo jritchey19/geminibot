00603cdf8990a7a0881b3c6f4441ff750cc3a7e9
jritchey19 <jritchey19@yahoo.com>
2026-05-04 17:41:33 +0000

    finished ch1-l5

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,12 +11,17 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY is not set")
     
-    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum." 
+    parser = argparse.ArgumentParser(
+        description="Gemini Chatbot"
+    )
+    parser.add_argument("user_prompt", type=str, help="User prompt to ask the Chatbot.")
+    args = parser.parse_args()
+
     client = genai.Client(api_key=api_key)
     
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=user_prompt
+        contents=args.user_prompt
     )
     
     if response.usage_metadata is None:
@@ -23,7 +30,7 @@ def main():
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
     
-    print(f"User prompt: {user_prompt}")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {prompt_tokens}")
     print(f"Response tokens: {response_tokens}")
     print("Response:")
